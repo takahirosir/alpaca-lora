c496dad3b53fbd53cb78ae6c32c81eda32fddc83
takahirosir <569603224@.com>
2023-07-20 07:58:41 +0000

    add llma-7b.sh and add ignore for models

---
+++ scripts/llama-7b.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source scripts/download.sh
+
+# Prepare dirs
+MODEL_DIR=models
+LLAMA_DIR=$MODEL_DIR/llama-7b
+mkdir -p $LLAMA_DIR
+
+# Download the llama delta weights
+download_all_urls $PWD/scripts/llama-7b.txt $LLAMA_DIR
